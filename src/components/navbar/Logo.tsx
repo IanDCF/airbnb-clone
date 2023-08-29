f972dbad7b754d69e0a100afaa4c0d6166bdcfb4
iandc <15554555-iandc@users.noreply.gitlab.com>
2023-08-28 18:50:12 -0700

    Create property listing categories selection ui

--- src/components/navbar/Logo.tsx
+++ src/components/navbar/Logo.tsx
@@ -1,9 +1,12 @@
 "use client";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const Logo = () => {
+  const router = useRouter();
   return (
     <Image
+      onClick={() => router.push("/")}
       alt="Logo"
       className="hidden md:block cursor-pointer"
       height="100"

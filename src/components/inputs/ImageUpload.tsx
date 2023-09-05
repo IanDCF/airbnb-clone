8ceec42ad8577e184d9ce3489b19bbdd5f65f1ad
ianhcl <ian.delcarpiofarfan@hcl.com>
2023-09-05 12:26:00 -0700

    Create RentModal.tsx component: step 4 image upload

---
+++ src/components/inputs/ImageUpload.tsx
@@ -0,0 +1,56 @@
+"use client";
+import { CldUploadWidget } from "next-cloudinary";
+import Image from "next/image";
+import { useCallback } from "react";
+import { TbPhotoPlus } from "react-icons/tb";
+
+declare global {
+  var cloudinary: any;
+}
+
+type Props = {
+  onChange: (value: string) => void;
+  value: string;
+};
+
+const ImageUpload: React.FC<Props> = ({ onChange, value }) => {
+  const handleUpload = useCallback(
+    (result: any) => {
+      onChange(result.info.secure_url);
+    },
+    [onChange]
+  );
+
+  return (
+    <CldUploadWidget
+      onUpload={handleUpload}
+      uploadPreset="wo7s5xqi"
+      options={{ maxFiles: 1 }}
+    >
+      {({ open }) => {
+        return (
+          <div
+            onClick={() => open?.()}
+            className="relative cursor-pointer hover:opacity-70 transition border-dashed border-2  rounded-md p-20 border-neutral-300 flex flex-col justify-center items-center gap-4 text-neutral-600"
+          >
+            <TbPhotoPlus size={50} cl />
+            <div className="font-semibold text-lg">Click to upload</div>
+            {value && (
+              <div className="absolute inset-0 w-full h-full">
+                <Image
+                  alt="Upload"
+                  fill
+                  style={{ objectFit: "cover" }}
+                  src={value}
+                  className="rounded-md"
+                />
+              </div>
+            )}
+          </div>
+        );
+      }}
+    </CldUploadWidget>
+  );
+};
+
+export default ImageUpload;

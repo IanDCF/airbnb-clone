320c2021fa2bf7b915b86f1c13834b3ce514b0ae
ianhcl <ian.delcarpiofarfan@hcl.com>
2023-09-09 11:46:03 -0700

    Fetch and render listings on home page

---
+++ src/components/commons/HeartButton.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { SafeUser } from "@/types/safeUser";
+import React from "react";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+
+type Props = {
+  listingId: string;
+  currentUser?: SafeUser | null;
+};
+
+const HeartButton: React.FC<Props> = ({ listingId, currentUser }) => {
+  const hasFavourited = false;
+  const toggleFavourite = () => {};
+  return (
+    <div
+      onClick={toggleFavourite}
+      className="relative hover:opacity-80 transition cursor-pointer"
+    >
+      <AiOutlineHeart
+        size={28}
+        className="fill-white absolute -top-[2px] -right-[2px]"
+      />
+      <AiFillHeart
+        size={24}
+        className={hasFavourited ? "fill-rose-500" : "fill-neutral-500/70"}
+      />
+    </div>
+  );
+};
+
+export default HeartButton;
